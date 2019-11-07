a7b493e955a37ade2306224f0cdb9cf4d3c4957e
Roman Roibu <romanroibu@gmail.com>
2019-11-07 14:05:56 +0100

    Define a command line interface for starting the host controller and pushing gaze data to the relay

---
+++ pupil_invisible/cli.py
@@ -0,0 +1,47 @@
+import abc
+import logging
+import typing
+
+import click
+
+from .host_controller import HostController
+from .pi_gaze_relay import PupilInvisibleGazeRelay
+
+
+@click.command()
+@click.option("--host-name", default=None, help="Device (host) name to connect")
+def main(host_name: str):
+
+    handlers = [
+        logging.StreamHandler(),
+    ]
+    logging.basicConfig(
+        level=logging.DEBUG,
+        handlers=handlers,
+        style="{",
+        format="{asctime} [{levelname}] {message}",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    logging.getLogger("pyre").setLevel(logging.WARNING)
+
+    if host_name is not None:
+        controller = HostController(host_name=host_name)
+    else:
+        raise NotImplementedError
+
+    gaze_relay = PupilInvisibleGazeRelay()
+
+    try:
+        while True:
+            controller.poll_events()
+            for gaze in controller.fetch_gaze():
+                # print(gaze)
+                gaze_relay.push_gaze_sample(gaze)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        controller.cleanup()
+
+
+if __name__ == "__main__":
+    main()
