2174ab56138c61ccdb17d5f74f1e1aa8c43905b2
Pablo Prietz <pablo@prietz.org>
2022-02-28 15:38:45 +0100

    Update license header

--- pupil_capture/pupil_capture_lsl_relay.py
+++ pupil_capture/pupil_capture_lsl_relay.py
@@ -1,7 +1,7 @@
 """
 (*)~----------------------------------------------------------------------------------
  Pupil LSL Relay
- Copyright (C) 2012-2016 Pupil Labs
+ Copyright (C) 2012 Pupil Labs
 
  Distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
  License details are in the file license.txt, distributed as part of this software.
